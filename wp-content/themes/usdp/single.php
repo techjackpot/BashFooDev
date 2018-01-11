3106c46df462e85353eb5354983345fec0dc74ba
Mr.Jack <erich.meyer117@gmail.com>
2018-01-10 22:15:50 -0500

    live specs parsing script

--- wp-content/themes/usdp/single.php
+++ wp-content/themes/usdp/single.php
@@ -10,11 +10,8 @@ $context['recentposts'] = Timber::get_posts(array(
 ));
 
 if(get_post_type() == 'equipment') {
-?>
-    
-
-<?php	$context['specs'] = explode(PHP_EOL, $context['post']->specs);
-	//$context['specifc'] =  do_shortcode($context['post']->specs);
+	
+	$context['specs'] = explode(PHP_EOL, $context['post']->specs);
 
 	$specific_html = '';
 	$spec_data = [];
@@ -22,15 +19,10 @@ if(get_post_type() == 'equipment') {
 	foreach($context['specs'] as $spec) {
 		if(!trim($spec)) continue;
 
-		preg_match_all("/\[SPECF (.+) ]/", $spec, $out);
+		preg_match_all("/([^-]+) -+ (.+)/", $spec, $out);
 
-		$data = $out[1][0];
-
-		$spec_params = preg_split("/\s{2,}|\|/", $data);
-
-		$spec_name = $spec_params[0];
-
-		unset($spec_params[0]);
+		$spec_name = $out[1][0];
+		$spec_params = preg_split("/\s{2,}|\|/", $out[2][0]);
 
 		$spec_params_data = [];
 		foreach($spec_params as $temp) {
@@ -40,8 +32,6 @@ if(get_post_type() == 'equipment') {
 		}
 
 		$spec_data[] = array('spec_name' => $spec_name, 'spec_params' => $spec_params_data);
-
-		// $specific_html .= '<div class="spec-row"><div class="spec-name">' . $spec_name . '</div>' . '<div class="spec-param">' . implode('</div><div class="spec-param">', $spec_params) . '</div>' . '</div>';
 
 		if(trim($spec_name) == 'MODEL') {
 			$model_names = $spec_params_data;
@@ -54,21 +44,16 @@ if(get_post_type() == 'equipment') {
 		foreach($spec['spec_params'] as $ind => $param) {
 			$specific_html2 .= '<div class="spec-param">' . ($spec['spec_name'] != 'MODEL' && count($spec['spec_params'])>1 ? '<span class="spec-param-extra">' . $model_names[$ind] . ': </span>' : '') . $param . '</div>';
 		}
-		// if(count($spec['spec_params'])<count($model_names)) {
-		// 	$specific_html2 .= str_repeat('<div class="spec-param"></div>', count($model_names) - count($spec['spec_params']));
-		// }
 		$specific_html2 .= '</div>';
-	// 		// <div class="spec-param">' . implode('</div><div class="spec-param">', $spec_params) . '</div>' . '</div>';
 	}
 
-	// $context['specifc'] = $specific_html;
 	$context['specifc2'] = $specific_html2;
 
 	$context['equipment_images'] = find_equipment_images($context['post']->custom['cat'], $context['post']->custom['class']);
 }
 
-	if($context['post']->custom['_thumbnail_id']) {
-		$context['featured_thumbnail'] = get_the_post_thumbnail($context['post']->ID);
-	}
+if($context['post']->custom['_thumbnail_id']) {
+	$context['featured_thumbnail'] = get_the_post_thumbnail($context['post']->ID);
+}
 
 Timber::render(array('singles/single-' . get_post_type() . '.twig', 'single.twig', 'index.twig'), $context);
